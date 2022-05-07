e4d2650f8b063aa134d69211b8115c01fbbfda1f
Irfan Mahmuf Maruf <67922561+Irfan-Mahmud-Maruf@users.noreply.github.com>
2022-05-07 15:49:53 +0600

    single male showing

--- meal.js
+++ meal.js
@@ -13,24 +13,86 @@ const loadMeal = () => {
 const displayMeal = (meals) => {
     // console.log(meals)
     meals.forEach(meal =>{
-        console.log(meal)
+        // console.log(meal)
         const row = document.getElementById('row');
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
         <div class="card">
-        <img src=${meal.strMealThumb} class="card-img-top" alt="..." />
-        <div class="card-body">
-          <h5 class="card-title">${meal.strMeal}</h5>
-          <p class="card-text">
-           ${meal.strInstructions.slice(0, 200)}
-          </p>
-          <button type="button" class="btn btn-success">Show Details</button>
-          <button type="button" class="btn btn-outline-success"><a href=${meal.strYoutube} target='_blank' style="text-decoration: none; color:green" onMouseOver="this.style.color='#fff'" onMouseOut="this.style.color='green'"> Recipe Video</a></button>
+            <img src=${meal.strMealThumb} class="card-img-top" alt="..." />
+            <div class="card-body">
+              <h5 class="card-title">${meal.strMeal}</h5>
+              <p class="card-text">
+               ${meal.strInstructions.slice(0, 200)}
+              </p>
+              <button type="button" class="btn btn-success" onclick='showDetails(${meal.idMeal})'>Show Details</button>
+              <button type="button" class="btn btn-outline-success"><a href=${meal.strYoutube} target='_blank' style="text-decoration: none; color:green" onMouseOver="this.style.color='#fff'" onMouseOut="this.style.color='green'"> Recipe Video</a></button>
 
+            </div>
         </div>
-      </div>
         `
         row.appendChild(div);
     })
+}
+
+const showDetails = (id) => {
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => showSingleMeal(data.meals[0]))
+}
+
+const showSingleMeal = (meal) =>{
+    console.log(meal)
+    const singleMeal = document.getElementById('single-meal');
+    const div = document.createElement('div');
+    div.classList.add('row');
+
+    div.innerHTML = `
+        <div class="col-md-4">
+            <img src=${meal.strMealThumb} class="img-fluid rounded-start" alt="..." />
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title">${meal.strMeal}</h5>
+
+            <div class="accordion accordion-flush" id="accordionFlushExample">
+              <div class="accordion-item">
+                <h2 class="accordion-header" id="flush-headingOne">
+                  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                    Description
+                  </button>
+                </h2>
+                <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+                  <div class="accordion-body">${meal.strInstructions}</div>
+                </div>
+              </div>
+              <div class="accordion-item">
+                <h2 class="accordion-header" id="flush-headingTwo">
+                  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+                  Category
+                  </button>
+                </h2>
+                <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
+                  <div class="accordion-body">${meal.strCategory}</div>
+                </div>
+              </div>
+              <div class="accordion-item">
+                <h2 class="accordion-header" id="flush-headingThree">
+                  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+                    Area
+                  </button>
+                </h2>
+                <div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
+                  <div class="accordion-body">${meal.strArea}</div>
+                </div>
+              </div>
+            </div>
+            
+            <button type="button" class="btn btn-outline-success w-100"><a href=${meal.strYoutube} target='_blank' style="text-decoration: none; color:green" onMouseOver="this.style.color='#fff'" onMouseOut="this.style.color='green'"> Recipe Video</a></button>
+
+          </div>
+        </div>
+    `
+    singleMeal.appendChild(div);
 }
